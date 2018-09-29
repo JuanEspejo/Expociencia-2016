7cbd223fa7c2037719a2dd41ebf245796286bb3b
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2018-09-29 11:10:32 -0500

    Update RareEvents.R

--- girsanov/RareEvents.R
+++ girsanov/RareEvents.R
@@ -1,17 +1,32 @@
+E_P = function(n,k){
+    x = numeric(k)
+    s = 0
+    d = n - k
+    for(i in 1:n){
+        z = g(rnorm(1,0,1))
+        s = s + z
+        if(i > d) x[i-d] = s/i
+    }
+    plot(x,type="l")
+    tail(x,1)
+}
+
 E_Q = function(n,k){
     x = numeric(k)
     s = 0
     d = n - k
     for(i in 1:n){
-        #if(i %% 1e5 == 0) print(sprintf("%5.1f%1s", 100*i/n,'%'))
-        z = rnorm(1,0,1)
-        if(z < 0) z = exp(6*z-18)
-        else z = 0
+        z = f(rnorm(1,6,1))
         s = s + z
         if(i > d) x[i-d] = s/i
     }
     plot(x,type="l")
-    tail(x,20)
+    tail(x,1)
+}
+
+g = function(x){
+      if(x < 0) return(exp(6*x-18))      
+      else return(0)
 }
 
 f = function(x){
